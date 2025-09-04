f33a44206d055590ed8de56b8fd597711c452e2a
d2scripts <servidor@lucas.srv.br>
2025-09-04 09:47:56 -0300

    Update monitor-data2.php

--- monitor/monitor-data2.php
+++ monitor/monitor-data2.php
@@ -2,6 +2,21 @@
 
 #DATA2-INTERNAL-CHECK-START
 
-print_r($_REQUEST);
+      #print_r($_REQUEST); #sleep(5);
+
+$R = [];
+
+$R['load'] = sys_getloadavg();
+
+$R['pong'] = md5($_REQUEST['ping'] ?? '');
+
+$R['services'] = [
+    'mysql' => intval((bool)  (fsockopen('localhost', 3306,$a,$b,2))  ),
+];
+
+
+header("Content-type: application/json");
+
+echo json_encode($R);
 
 #DATA2-INTERNAL-CHECK-END
